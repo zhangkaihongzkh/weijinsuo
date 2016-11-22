297516ec8ff47f8ea1da16f11b7fe1590ea90fb8
zhangkaihongzkh <953628520@qq.com>
2016-11-22 13:00:51 +0800

    增加了导航栏吸顶的效果 完善了选项卡切换样式

--- js/main.js
+++ js/main.js
@@ -24,11 +24,23 @@ $(function(){
 
 	/*动态设置标签页横向滚动条ul宽度*/
 	var $navTabs = $('ul.nav-tabs');
-	var width = 30;	//原先ul有padding-left10px
+	var navTabsWidth = 30;	//原先ul有padding-left10px
 	$navTabs.children().each(function(index,element){
-		width += element.clientWidth;
+		navTabsWidth += element.clientWidth;
 	});
-	$navTabs.css({'width':width});
-
+	if(navTabsWidth > $(window).width()){
+		$navTabs.css({'width':navTabsWidth}).parent().css({'overflow-x':'scroll'});
+	}
 	
+
+	//纵向选项卡绑定事件
+	var $newsTitle = $('.news-title');//获取到标题对象
+	$('#news .news-tab > li > a').on('click',function(){
+		//1.获取到当前dom节点，转为为jq对象
+		var $this = $(this);
+		//2.获取到data-title值
+		var title = $this.data('title');
+		//3.设置title的值
+		$newsTitle.text(title);
+	});
 });
